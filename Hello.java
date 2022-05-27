c83feaa707d374a24685c9bbcf6a6973cb9971c7
unknown <patildhiraj86933@gmail.com>
2022-05-27 17:15:10 +0530

    Hello.java created

---
+++ Hello.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+	{
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Hello()
+		{
+		}
+         void studentMethod()
+        {
+            System.out.println("Well Done!");
+        }
+		
+	}
+}
+     
+	class Hello
+    {
+		public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        // Solution.s1=new Solution();//
+        // s1.studentMethod();
+		
+    }
+    }
+
+    
+    }
+    
+}
